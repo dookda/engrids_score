acac09619a527c1049e0b1efebdbd3d4da33b059
sakda homhuan <sakda.homhuan@gmail.com>
2023-01-30 08:26:43 +0700

    update

--- nodejs/www/score/app.js
+++ nodejs/www/score/app.js
@@ -45,25 +45,38 @@ let gotoIndex = () => {
     location.href = "./../index.html";
 }
 
+let gotoReport = () => {
+    location.href = "./../report/index.html";
+}
+
 if (token) {
     axios.post("/scoreapi/getscore", { token }).then(r => {
         r.data.data.forEach(i => {
             document.getElementById("scorelist").innerHTML = `<div class="card mb-2">
-            <div class="card-body">
-                <div class="d-flex justify-content-between mb-2">
-                    <div class="lable">คะแนนเก็บ รายวิชา ${i.sub_code} ${i.sub_name} section ${i.sub_sect} : ${i.score1}  คะแนน</div>
+                <div class="card-body">
+                    <div class="d-flex justify-content-between mb-2">
+                        <div class="lable">คะแนนเก็บ รายวิชา ${i.sub_code} ${i.sub_name} section ${i.sub_sect} : ${i.score1}  คะแนน</div>
+                    </div>
                 </div>
-            </div>
-        </div>`
+            </div>`
         });
 
-
-
-        document.getElementById("profile").innerHTML = `<a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+        if (r.data.info.itaccounttype_TH == "บุคลากร") {
+            document.getElementById("profile").innerHTML = `<a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
             aria-expanded="false"> <span class="form-label">${r.data.info.firstname_TH} ${r.data.info.lastname_TH}</span></a>
             <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#" onclick="gotoInput()">กรอกคะแนน</a></li>
+                <li><a class="dropdown-item" href="#" onclick="gotoReport()">ดูคะแนน</a></li>
                 <li><a class="dropdown-item" href="#" onclick="gotoLogout()">logout</a></li>
+        </ul>`
+        } else {
+            document.getElementById("profile").innerHTML = `<a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                aria-expanded="false"> <span class="form-label">${r.data.info.firstname_TH} ${r.data.info.lastname_TH}</span></a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#" onclick="gotoLogout()">logout</a></li>
             </ul>`
+        }
+
     });
 } else {
     document.getElementById("profile").innerHTML = `<button class="btn btn-info" onclick="gotoLogin()" >เข้าสู่ระบบ</button>`
