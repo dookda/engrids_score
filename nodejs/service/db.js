7fb68e3b7192bed25aa980da0cfeea81784e2d8f
dookda <sakda.homhuan@gmail.com>
2024-09-04 20:50:09 +0700

    update

--- nodejs/service/db.js
+++ nodejs/service/db.js
@@ -1,10 +1,18 @@
 const Pool = require('pg').Pool
 
+// const datapool = new Pool({
+//     user: 'postgres',
+//     host: 'localhost',
+//     database: 'scoredb',
+//     password: 'Eec-MIS2564db',
+//     port: 5432,
+// });
+
 const datapool = new Pool({
     user: 'postgres',
-    host: 'localhost',
+    host: 'postgres',
     database: 'scoredb',
-    password: 'Eec-MIS2564db',
+    password: '1234',
     port: 5432,
 });
 
